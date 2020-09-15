a1c95b8d8e697ea49dc5364497785b995cadee5c
zephyrus21 <pandey.piyus@gmail.com>
2020-09-15 11:48:23 +0530

    build test

---
+++ build/precache-manifest.20cde741b5056c806109da42fd33e49f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fb44d2f9330b3b5316f3964aabed36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0e50061ad35664a65f",
+    "url": "/static/css/main.4c169b65.chunk.css"
+  },
+  {
+    "revision": "dd4ea6cacb8e816f6b3d",
+    "url": "/static/js/2.7e3c39e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e3c39e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0e50061ad35664a65f",
+    "url": "/static/js/main.e31e054e.chunk.js"
+  },
+  {
+    "revision": "8662a9d4b7393fb41e69",
+    "url": "/static/js/runtime-main.72dcf36d.js"
+  }
+]);
